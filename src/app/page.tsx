8a54f3b2bcbb356d6b6494f6c0e9ab53dcb1d2b3
simoesbone-code <simoesbone@gmail.com>
2026-01-26 21:53:25 -0300

    Botao whatsapa add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import { Banner } from "@/components/Banner";
 import { Section } from "@/components/Section";
 import { ProductGrid } from "@/components/ProductGrid";
 import { PropsProduct } from "@/types/product";
+import { WhatsAppFloatingButton } from "@/components/WhatsAppFloatingButton";
 
 export default function Home() {
   const { data: dataProduct } = useQuery<PropsProduct[]>({
@@ -20,6 +21,11 @@ export default function Home() {
       <Banner />
       <Section title="Todos os Bonés" />
       <ProductGrid dataProduct={dataProduct} />
+
+      <WhatsAppFloatingButton
+        phone="5583998033753"
+        message="Olá! Vim pelo site e gostaria de saber mais."
+      />
     </>
   );
 }
